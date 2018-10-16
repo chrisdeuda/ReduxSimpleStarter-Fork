b087a1ebfc9940bc6a1104fcad233a5b5fb68ef0
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-17 01:22:59 +0800

    Refactor main components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = process.env.API_YOUTUBE_URL;
 
-// Create component. This component should some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = { videos: []};
+
+        YTSearch({ key: API_KEY, term: 'surfboardss'} , (data) => {
+            this.setState({videos: data});
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Take this component's generated HTML and put it 
